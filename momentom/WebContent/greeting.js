ba416bfeeb1881c6c6183f529783cac9edbe16f4
ilfm <dnehd1828@gmail.com>
2020-12-21 18:42:22 +0900

    20.12.21 momentom gretting.js add

---
+++ momentom/WebContent/greeting.js
@@ -0,0 +1,39 @@
+/**
+ * greeting.js
+ * - 날짜 : 20.12.21
+ * - 설명 : 사용자 이름 저장하는 js
+ */
+
+const form = document.querySelector(".js-form");
+	input = document.querySelector("input");
+// 1.querySelector 는
+// 해당하는 요소의 첫번째 Element를 반환한다.
+// https://developer.mozilla.org/ko/docs/Web/API/Document/querySelector
+// 2.querySelectorAll은
+// 지정된 셀렉터 그룹에 일치하는 ElementList → NodeList를 반환한다.
+
+function loadName()
+{
+	
+}
+	
+	
+function init()
+{
+	loadName();
+}
+
+init();
+
+
+
+// localStrage 란?
+/*
+	1. localStorage.setItem(param,param)
+	2. localStorage.getItem(param,param)
+
+
+
+
+
+*/
